299373be33b62cbae4a1cc6cb683617c12e19aad
windxmax <veterneustanet@gmail.com>
2018-10-05 00:46:31 +0300

    Генератор градиентов для фона

--- src/App.js
+++ src/App.js
@@ -1,28 +1,58 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import ReactDOM from 'react-dom'
 
-class App extends Component {
+export default class App extends Component {
+  constructor(props) {
+    super(props)
+  }
+  state = {
+    firstColor: '',
+    secondColor: '',
+    spoiler: 'Введите цвета:'
+  }
+  onInput = (e) => {
+    const {id, value} = e.currentTarget
+    this.setState({[id]: value})
+  }
+
+  colorChange = (e) =>{
+    e.preventDefault()
+    const {firstColor, secondColor} = this.state
+    if (firstColor.trim() && secondColor.trim()){
+      let area = ReactDOM.findDOMNode(this.refs.root)
+      area.style.background = `linear-gradient(180deg, ${firstColor}, ${secondColor})`
+      this.setState({spoiler: 'Введите цвета:'})
+    }else{
+      this.setState({spoiler: 'Неверный ввод!'})
+    }
+  }
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
+      <div className='root' ref = {'root'}>
+        <form className="App_main_window">
+          <h5 className='App_main_window_hedaerText'>{this.state.spoiler}</h5>
+          <input
+            id='firstColor'
+            onChange={this.onInput}
+            className='App_main_window_colorInput'
+            placeholder='Первый цвет'
+            autocomplete="off"
+          />
+          <input
+            id='secondColor'
+            onChange={this.onInput}
+            className='App_main_window_colorInput'
+            placeholder='Второй цвет'
+            autocomplete="off"
+          />
+          <button
+            onClick={this.colorChange}
+            className='App_main_window_buttonStart'
           >
-            Learn React
-          </a>
-        </header>
+          Поехали
+          </button>
+        </form>
       </div>
     );
   }
 }
-
-export default App;
